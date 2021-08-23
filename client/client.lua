10d29afb45c8d16c651c4ab2207a26a5b97a2b9e
ImClod <clod.st@outlook.com>
2021-08-23 22:38:15 +0200

    fixed ESX errors

--- client/client.lua
+++ client/client.lua
@@ -1,16 +1,18 @@
 RegisterNetEvent('fnx-setPed')
 AddEventHandler('fnx-setPed',function(ped)
+    Citizen.Wait(2500)  
+    local playerPed = PlayerPedId()
+    local characterModel = GetHashKey(ped)
+    RequestModel(characterModel)   
     Citizen.CreateThread(function()
-  
-      while not HasModelLoaded(ped) do
-        RequestModel(ped)
-        Citizen.Wait(0)
-      end
-  
-      if IsModelInCdimage(ped) and IsModelValid(ped) then
-        SetPlayerModel(PlayerId(), ped)
-        SetPedDefaultComponentVariation(playerPed)
-      end
-      SetModelAsNoLongerNeeded(ped)
+        while not HasModelLoaded(characterModel) do
+            RequestModel(characterModel)
+            Citizen.Wait(0)
+        end
+        if IsModelInCdimage(characterModel) and IsModelValid(characterModel) then
+            SetPlayerModel(PlayerId(), characterModel)
+            SetPedDefaultComponentVariation(playerPed)
+        end
+        SetModelAsNoLongerNeeded(characterModel)
     end)
 end)
